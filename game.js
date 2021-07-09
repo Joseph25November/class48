c4803e5e40b57f3997445ad37872ddf4f7a20bf4
Joseph25November <74719907+Joseph25November@users.noreply.github.com>
2021-07-09 18:57:52 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,160 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+  cat = createSprite(200,displayHeight / 2 - 100,50,50)
+  cat.scale = 0.3
+  mouse = createSprite(displayWidth / 2 - 300,100,30,30)
+  mouse.scale = 0.3
+  cat.addImage("cat", catimage)
+  mouse.addImage("mouse", mouseimage)
+  cars = [cat, mouse];
+  cat.debug=true;
+  mouse.debug=true;
+cat.setCollider("rectangle",200,0,1000,1000)
+  
+  }
+
+  play(){
+
+   form.hide();
+  image(Stadium,0,0,displayWidth,displayHeight);
+    Player.getPlayerInfo();
+  if(allPlayers !== undefined){
+    background(rgb(198,135,103));
+    image(Stadium,0,0,displayWidth,displayHeight);
+    
+    //var display_position = 100;
+    
+    //index of the array
+    var index = 0;
+
+    //x and y position of the cars
+    var x = 175 ;
+    var y;
+
+    for(var plr in allPlayers){
+      //add 1 to the index for every loop
+      index = index + 1 ;
+
+      //position the cars a little away from each other in x direction
+      x = allPlayers[plr].x;
+      //use data form the database to display the cars in y direction
+      y = allPlayers[plr].y;
+      cars[index-1].x = x;
+      cars[index-1].y = y;
+  
+
+     
+      if (index === player.index){
+        if(player.index === 1){
+      cars[index-1].x = 100;
+      cars[index-1].y = 200;
+        }
+        if(player.index === 2){
+          cars[index-1].x = displayWidth - 120;
+          cars[index-1].y = 200;
+            }
+        stroke(10);
+        fill("red");
+        ellipse(x,y,60,60);
+        cars[index - 1].shapeColor = "red";
+
+        if(keyIsDown(32)){
+          var bullet= createSprite(cars[index-1].x,cars[index-1].y,50,10);
+          }
+
+         
+       
+      }
+
+      if(keyIsDown(70)&& cat.isTouching(mouse)&&player.index===1){
+        //cat.addImage("cat", catattack)
+        player.health -=20;
+        player.x=100;
+        player.y=200;
+        player.update();
+        }
+
+        if(keyIsDown(68)&& mouse.isTouching(cat)&&player.index===2){
+          //mouse.addImage("mouse", mouseattack)
+          player.health -=20;
+          player.x = displayWidth - 150;
+          player.y=200;
+          player.update()
+          }
+     
+      textSize(15);
+      text(allPlayers[plr].name + ": " + allPlayers[plr].health, 120,20)
+    }
+
+  }
+  
+     if(keyIsDown(UP_ARROW)){
+      player.y -=5
+      player.update()
+      }
+
+      
+
+      if(keyIsDown(LEFT_ARROW)){
+        player.x -=5
+        player.update()
+        }
+
+        if(keyIsDown(DOWN_ARROW)){
+          player.y +=5
+          player.update()
+          }
+
+          if(keyIsDown(RIGHT_ARROW)){
+            player.x +=5
+            player.update()
+            }
+
+            
+
+           
+/*
+            if(keyPressed("E")&& mouse.isTouching){
+              cat.addImage("mouseattack", mouseattack)
+              player.health -=20
+              }
+
+              if(player.health === 0){
+              image(GameOver, displayWidth / 2,displayHeight / 2, displayWidth,displayHeight)
+              }*/
+
+              drawSprites();
+  }
+  end(){
+    console.log("Game Ended");
+    console.log(player.rank)
+  }
+}
